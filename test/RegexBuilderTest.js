dd63b5f75d59c4cccbffda5911e65b1eefa67223
Dean Gaffney <deanpatrickgaffney@gmail.com>
2020-07-04 15:30:52 +0100

    added spacing functions, renamed functions and added character range functionality

--- test/RegexBuilderTest.js
+++ test/RegexBuilderTest.js
@@ -13,14 +13,14 @@ describe.only('Unit: RegexBuilder', () => {
   it('should be possible to build a regex with a number passed as a string', () => {
     const builder = new RegexBuilder();
     const regex = builder.number('1').toRegExp();
-    assert.strictEqual(builder.toRegexString(), '1');
+    assert.strictEqual(builder.toRegexString(), '\\d');
     assert(regex.exec('1'), 'should have matched');
   });
 
   it('should be possible to build a regex with a number passed as a number', () => {
     const builder = new RegexBuilder();
     const regex = builder.number(1).toRegExp();
-    assert.strictEqual(builder.toRegexString(), '1');
+    assert.strictEqual(builder.toRegexString(), '\\d');
     assert(regex.exec('1'), 'should have matched');
   });
 
@@ -63,9 +63,9 @@ describe.only('Unit: RegexBuilder', () => {
     assert('https://'.match(regex), 'should have matched');
   });
 
-  it('should be possible to build a regex with "or" syntax from a pattern', () => {
+  it('should be possible to build a regex with OR syntax from a pattern', () => {
     const builder = new RegexBuilder();
-    const regex = builder.startsWith('hello').followedBy(' world').or('there');
+    const regex = builder.startsWith('hello').then(' world').or('there');
     assert.strictEqual(builder.toRegexString(), '^hello world|there');
     assert('hello world'.match(regex), 'should have matched');
     assert('hello there'.match(regex), 'should have matched');
@@ -113,7 +113,7 @@ describe.only('Unit: RegexBuilder', () => {
       .optional('1')
       .then('world')
       .toRegExp();
-    assert.strictEqual(builder.toRegexString(), '1?world');
+    assert.strictEqual(builder.toRegexString(), '(?:1)?world');
     assert('1world'.match(regex), 'should have matched');
     assert('world'.match(regex), 'should have matched');
   });
@@ -124,7 +124,7 @@ describe.only('Unit: RegexBuilder', () => {
       .maybe('1')
       .then('world')
       .toRegExp();
-    assert.strictEqual(builder.toRegexString(), '1?world');
+    assert.strictEqual(builder.toRegexString(), '(?:1)?world');
     assert('1world'.match(regex), 'should have matched');
     assert('world'.match(regex), 'should have matched');
   });
@@ -158,21 +158,12 @@ describe.only('Unit: RegexBuilder', () => {
       .caseSensitive(false)
       .toRegExp();
     assert('1world'.match(/1WORld/i), 'should have matched');
-    assert.strictEqual(builder.toRegexString(), '1?woRLd');
+    assert.strictEqual(builder.toRegexString(), '(?:1)?woRLd');
     assert('1world'.match(regex), 'should have matched');
     assert('WOrld'.match(regex), 'should have matched');
   });
 
   it('should be possible to build a regex that matches anything but the given values', () => {
-    const builder = new RegexBuilder();
-    const regex = builder.anythingBut('abc')
-      .toRegExp();
-    assert.strictEqual(builder.toRegexString(), '[^abc]');
-    assert(!'abc'.match(regex), 'should not have matched');
-    assert('def'.match(regex), 'should have matched');
-  });
-
-  it('should be possible to build a regex that matches anything but the given values as an array', () => {
     const builder = new RegexBuilder();
     const regex = builder.anythingBut(['a', 'b', 'c'])
       .toRegExp();
@@ -183,7 +174,7 @@ describe.only('Unit: RegexBuilder', () => {
 
   it('should be possible to build a regex that matches a pattern not followed by another pattern', () => {
     const builder = new RegexBuilder();
-    const regex = builder.startsWith('abc').notFollowedBy('def')
+    const regex = builder.startsWith('abc').mustNotBeFollowedBy('def')
       .toRegExp();
     assert.strictEqual(builder.toRegexString(), '^abc(?!def)');
     assert(!'abcdef'.match(regex), 'should not have matched');
@@ -197,7 +188,127 @@ describe.only('Unit: RegexBuilder', () => {
       .toRegExp();
     assert.strictEqual(builder.toRegexString(), '\\w+');
     assert(!'*'.match(regex), 'should not have matched');
-    assert('hello'.match(regex), 'should not have matched');
+    assert('hello'.match(regex), 'should have matched');
     assert('abc-some-other-pattern'.match(regex), 'should have matched');
+  });
+
+  it('should be possible to repeat a pattern a given number of times', () => {
+    const builder = new RegexBuilder();
+    const regex = builder.repeat('hello', 3)
+      .toRegExp();
+    assert.strictEqual(builder.toRegexString(), '(?:hello){3}');
+    assert('hellohellohello'.match(regex), 'should have matched');
+    assert(!'hello'.match(regex), 'should have matched');
+    assert(!'hellohello'.match(regex), 'should not have matched');
+  });
+
+  it('should be possible to generate a regex with an OR condition using "eitherOf"', () => {
+    const builder = new RegexBuilder();
+    const regex = builder.eitherOf(['hello', 'world'])
+      .toRegExp();
+    assert.strictEqual(builder.toRegexString(), '(?:hello|world)');
+    assert('hello'.match(regex), 'should have matched');
+    assert('world'.match(regex), 'should have matched');
+  });
+
+  it('should be possible to generate a regex with a positive look ahead', () => {
+    const builder = new RegexBuilder();
+    const regex = builder.then('hello').space().mustBeFollowedBy('something')
+      .toRegExp();
+    assert.strictEqual(builder.toRegexString(), 'hello (?=something)');
+    assert('hello something'.match(regex), 'should have matched');
+    assert(!'hello'.match(regex), 'should not have matched');
+  });
+
+  it('should be possible to generate a regex with a space', () => {
+    const builder = new RegexBuilder();
+    const regex = builder.then('hello').space().then('something')
+      .toRegExp();
+    assert.strictEqual(builder.toRegexString(), 'hello something');
+    assert('hello something'.match(regex), 'should have matched');
+  });
+
+  it('should be possible to generate a regex that can match a single whitespace', () => {
+    const builder = new RegexBuilder();
+    const regex = builder.then('hello').whitespace().then('something')
+      .toRegExp();
+    assert.strictEqual(builder.toRegexString(), 'hello\\ssomething');
+    assert('hello\tsomething'.match(regex), 'should have matched');
+  });
+
+  it('should be possible to generate a regex that can match a single tab', () => {
+    const builder = new RegexBuilder();
+    const regex = builder.then('hello').tab().then('something')
+      .toRegExp();
+    assert.strictEqual(builder.toRegexString(), 'hello\\tsomething');
+    assert('hello\tsomething'.match(regex), 'should have matched');
+  });
+
+  it('should be possible to generate a regex that can match a new line', () => {
+    const builder = new RegexBuilder();
+    const regex = builder.then('hello').newLine().then('something')
+      .toRegExp();
+    assert.strictEqual(builder.toRegexString(), 'hello(?:\\r\\n|\\r|\\n)something');
+    assert('hello\nsomething'.match(regex), 'should have matched');
+  });
+
+  it('should be possible to generate a regex that can match an uppercase letter', () => {
+    const builder = new RegexBuilder();
+    const regex = builder.uppercaseLetter().toRegExp();
+    assert.strictEqual(builder.toRegexString(), '[A-Z]');
+    assert('H'.match(regex), 'should have matched');
+    assert(!'h'.match(regex), 'should not have matched');
+  });
+
+  it('should be possible to generate a regex that can match a lowercase letter', () => {
+    const builder = new RegexBuilder();
+    const regex = builder.lowercaseLetter().toRegExp();
+    assert.strictEqual(builder.toRegexString(), '[a-z]');
+    assert('h'.match(regex), 'should have matched');
+    assert(!'H'.match(regex), 'should not have matched');
+  });
+
+  it('should be possible to generate a regex using "followedBy"', () => {
+    const builder = new RegexBuilder();
+    const regex = builder.match('hello').space().followedBy('world').toRegExp();
+    assert.strictEqual(builder.toRegexString(), 'hello world');
+    assert('hello world'.match(regex), 'should have matched');
+  });
+
+  it('should be possible to generate a regex to match multiple numbers', () => {
+    const builder = new RegexBuilder();
+    const regex = builder.numbers().toRegExp();
+    assert.strictEqual(builder.toRegexString(), '\\d+');
+    assert('1'.match(regex), 'should have matched');
+    assert('1234'.match(regex), 'should have matched');
+  });
+
+  it('should be possible to generate a regex to match a character range', () => {
+    const builder = new RegexBuilder();
+    const regex = builder.characterRange('a', 'd').toRegExp();
+    assert.strictEqual(builder.toRegexString(), '[a-d]');
+    assert('a'.match(regex), 'should have matched');
+    assert('b'.match(regex), 'should have matched');
+    assert('c'.match(regex), 'should have matched');
+    assert('d'.match(regex), 'should have matched');
+    assert(!'e'.match(regex), 'should not have matched');
+  });
+
+  it('should throw an error if the starting character is larger than the ending character in a character range', () => {
+    const builder = new RegexBuilder();
+    assert.throws(() => builder.characterRange('d', 'a'), {
+      name: 'Error',
+      message: 'Start of character range'
+      + ' is greater than the end character, make sure both characters are the same casing.'
+    });
+  });
+
+  it('should throw an error if the the characters for a character range are different casings', () => {
+    const builder = new RegexBuilder();
+    assert.throws(() => builder.characterRange('a', 'B'), {
+      name: 'Error',
+      message: 'Start of character range'
+      + ' is greater than the end character, make sure both characters are the same casing.'
+    });
   });
 });
